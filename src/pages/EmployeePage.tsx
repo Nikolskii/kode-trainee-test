a44e36bf83c70b295b543012eaafb338f09e737c
Denis <den-apple@ya.ru>
2023-02-25 17:04:35 +0200

    chore: создать заготовки страниц

---
+++ src/pages/EmployeePage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EmployeePage = () => {
+  return <div>EmployeePage</div>;
+};
+
+export default EmployeePage;
---
+++ src/pages/EmployeesPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EmployeesPage = () => {
+  return <div>EmployeesPage</div>;
+};
+
+export default EmployeesPage;
