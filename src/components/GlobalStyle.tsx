bfa4d73a3c37a345303f9f14b057539f00c98230
Denis <den-apple@ya.ru>
2023-02-25 15:38:40 +0200

    feat: создать компонент GlobalStyle

---
+++ src/components/GlobalStyle.tsx
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+
+import FontStyles from './FontStyles';
+
+const Styles = createGlobalStyle`
+  :root {
+    /* Typography */
+    --family: 'Inter', Arial, sans-serif;
+    --text-color-primary: #050510;
+    --text-color-secondary-light: #97979B;
+    --text-color-secondary-dark: #55555C;
+
+    /* Colors */
+    --colors-bg: #fff;
+
+    /* Sizes */
+    --sizes-min: 320px;
+    --sizes-max: 1280px;
+  }
+
+  body {
+    font-family: var(--family);
+    background: var(--colors-bg);
+    min-width: var(--sizes-min);
+    max-width: var(--sizes-max);
+    margin: 0 auto;
+  }
+`;
+
+const GlobalStyle = () => {
+  return (
+    <>
+      <Normalize />
+      <FontStyles />
+      <Styles />
+    </>
+  );
+};
+
+export default GlobalStyle;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 // import { store } from './app/store';
 import App from './App';
+import GlobalStyle from './components/GlobalStyle';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -10,6 +11,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
+    <GlobalStyle />
     <App />
     {/* </Provider> */}
   </React.StrictMode>,
